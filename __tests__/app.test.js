a78baddd02e76b2ee590e12331475b66c0aa868c
Katie <katieberrym@gmail.com>
2021-01-06 14:48:19 -0800

    added test for verify

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const { globalAgent } = require('https');
+
 
 describe('tardygram-app routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
-  it('allows a user to sign up via POST', async() => {
+  it('allows a user to sign up via POST', async () => {
     return request(app)
       .post('/api/v1/auth/signup')
       .send({
@@ -16,14 +18,14 @@ describe('tardygram-app routes', () => {
         password: 'password'
       })
       .then(res => {
-        expect (res.body).toEqual({
-          email: 'test@test.com',
-          password: 'password'
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          email: 'test@test.com'
         });
       });
   });
 
-  it('allows a user to login via POST', async() => {
+  it('allows a user to login via POST', async () => {
     const user = await UserService.create({
       email: 'test@test.com',
       password: 'password'
@@ -38,8 +40,30 @@ describe('tardygram-app routes', () => {
 
     expect(res.body).toEqual({
       id: user.id,
+      email: 'test@test.com'
+    });
+  });
+
+  it('verifies a user is logged-in via GET', async () => {
+    const agent = request.agent(app);
+    const user = await UserService.create({
       email: 'test@test.com',
       password: 'password'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const res = await agent
+      .get('/api/v1/auth/verify');
+
+    expect(res.body).toEqual({
+      id: user.id,
+      email: 'test@test.com'
     });
   });
 
