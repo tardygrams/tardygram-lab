4f6480e07c4da6d520a601f3453b82e22b53375a
Perry Sittser <sittserp@gmail.com>
2021-01-06 14:18:25 -0800

    signup test failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,20 @@ describe('tardygram-app routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
+
+  it('allows a user to sign up via POST', async() => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+      .then(res => {
+        expect (res.body).toEqual({
+          email: 'test@test.com',
+          password: 'password'
+        });
+      });
+  }
+  );
 });
