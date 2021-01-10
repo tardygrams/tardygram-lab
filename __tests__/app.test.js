66c484c6328543dbcea203b14b57520fb3207c18
Perry Sittser <sittserp@gmail.com>
2021-01-10 09:38:25 -0800

    seed file working. tests need to be aligned

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,12 +4,17 @@ const request = require('supertest');
 const app = require('../lib/app');
 const { globalAgent } = require('https');
 const UserService = require('../lib/services/UserService');
+const Post = require('../lib/models/Post');
 
 
 describe('tardygram-app routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/populate-comments.sql', 'utf-8'));
+  });
+
 
   it('allows a user to sign up via POST', async() => {
     return request(app)
@@ -79,12 +84,10 @@ describe('tardygram-app routes', () => {
     });
   });
 
-
   // what if there is no comment? NULL?
   it('responds with a list of the 10 posts with most comments via GET', async() => {
-    await pool.query(fs.readFileSync('sql/populate-comments.sql', 'utf-8'));
-    const res = request(app)
-      .get('/top-posts');
+    const res = await request(app)
+      .get('/api/v1/auth/top-posts');
     expect(res.body).toEqual([
       { id: 1, photo_url: 'someurl.url', caption:'some caption', tags: '#sometag', user_id: 1, count: 10 },
       { id: 2, photo_url: 'someurl.url', caption:'some caption', tags: '#sometag', user_id: 1, count: 2 },
@@ -99,4 +102,29 @@ describe('tardygram-app routes', () => {
 
     ]);
   });
+
+  it('tests the add comment route', async() => {
+    const post = await Post.insert({
+      user_id: '1',
+      photo_url: 'photo.com',
+      caption: 'lovely',
+      tags: '#nofilter'
+    });
+    const res = await request(app)
+      .insert('/comments')
+      .send({
+        comment_by: post.user_id,
+        post_id: 1,
+        comment: 'nice'
+      });
+      
+    expect(res.body).toEqual({
+      comment_by: post.user_id,
+      post_id: 1,
+      comment: 'nice'
+    });
+
+
+  });
+
 });
--- sql/populate-comments.sql
+++ sql/populate-comments.sql
@@ -16,35 +16,36 @@ INSERT INTO users (email,
 INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('1', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
 INSERT INTO comments (post_id, comment_by, comment) VALUES ('1', '2', 'coool post');
 
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('1', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('2', '2', 'coool post');
 
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('2', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('1', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('3', '2', 'coool post');
 
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('1', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('4', '2', 'coool post');
 
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('3', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('1', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('5', '2', 'coool post');
 
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('4', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('2', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('6', '2', 'coool post');
 
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('2', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('7', '2', 'coool post');
 
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('5', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('2', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('8', '2', 'coool post');
 
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('2', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('9', '2', 'coool post');
 
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('6', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('2', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('10', '2', 'coool post');
 
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('2', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('11', '2', 'coool post');
 
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('7', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
-
-
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('8', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
-
-
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('9', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
-
-
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('10', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
-
-
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('11', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
-
-
---INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('12', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO posts (user_id, photo_url, caption, tags) VALUES ('2', 'post.photo.url', 'great photo', '{"tags": "#nofilter"}');
+INSERT INTO comments (post_id, comment_by, comment) VALUES ('12', '2', 'coool post');
 
